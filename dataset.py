00874ca7e2c780c1222afc8facf67fe7f9be1d71
wdy06 <mituhashitakashi@gmail.com>
2019-11-27 01:28:26 +0000

    add debug mode

--- dataset.py
+++ dataset.py
@@ -8,8 +8,10 @@ class AshraeDataset():
         self.mode = mode
         self.debug = debug
 
-        self.building_meta = pd.read_csv(utils.DATA_DIR / 'building_meta.csv')
+        self.building_meta = pd.read_csv(utils.DATA_DIR / 'building_metadata.csv')
         self.main_df = pd.read_csv(utils.DATA_DIR / f'{mode}.csv')
+        if self.debug and self.mode=='train':
+            self.main_df = self.main_df[:2000]
         self.weather = pd.read_csv(utils.DATA_DIR / f'weather_{mode}.csv')
 
         self.main_df['timestamp'] = pd.to_datetime(self.main_df['timestamp'])
@@ -19,9 +21,9 @@ class AshraeDataset():
                                   on='building_id', how='left')
         self.merged_df = pd.merge(self.merged_df, self.weather, 
                                   on=['site_id', 'timestamp'], how='left')
-                                  
+
 
     def __len__(self):
         return len(self.main_df)
 
-        
+
